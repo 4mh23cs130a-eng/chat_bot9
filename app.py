2973c4d01dda4029797930a3faeffdf4bbb4080d
sahana <4mh13cs130a@gmail.com>
2026-02-05 15:42:59 +0530

    chat-bot

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
-from ai import get_ai_response
+from flask import Flask
 
-print("Welcome to the AI Chat Bot!")
-print("Type 'exit' or 'quit' to end the conversation.")
+app = Flask(__name__)
 
-while True:
-    user_input = input("You: ")
-    if user_input.lower() in ["exit", "quit"]:
-        break
-    response = get_ai_response(user_input)
-    print("AI:", response)
+
+app.route('/')
+def index():
+    return "Hello World!"
+
+# from ai import get_ai_response
+
+# print("Welcome to the AI Chat Bot!")
+# print("Type 'exit' or 'quit' to end the conversation.")
+
+# while True:
+#     user_input = input("You: ")
+#     if user_input.lower() in ["exit", "quit"]:
+#         break
+#     response = get_ai_response(user_input)
+#     print("AI:", response)
+
+if __name__ == "__main__":
+    app.run(debug=True)
